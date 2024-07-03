5877df187c78bc652a3deb94bd79bc83c020bdce
Molly <mollyshewchuk@gmail.com>
2024-07-03 08:42:40 -0600

    Audio recording

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -12,6 +12,7 @@ import {Popover} from "@headlessui/react";
 import {isAudio, isImage} from "@/helpers.js";
 import CustomAudioPlayer from "@/Components/App/CustomAudioPlayer.jsx";
 import AttachmentPreview from "@/Components/App/AttachmentPreview.jsx";
+import AudioRecorder from "@/Components/App/AudioRecorder.jsx";
 
 const MessageInput = ({conversation = null }) => {
     const [newMessage, setNewMessage] = useState("");
@@ -96,6 +97,10 @@ const MessageInput = ({conversation = null }) => {
         });
     }
 
+    const recordedAudioReady = (file, url) => {
+        setChosenFiles((prevFiles) => [...prevFiles, {file, url}]);
+    }
+
     return (
         <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
             <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
@@ -118,6 +123,7 @@ const MessageInput = ({conversation = null }) => {
                         className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
                     />
                 </button>
+                <AudioRecorder fileReady={recordedAudioReady} />
             </div>
             <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
                 <div className="flex">
